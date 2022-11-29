b65e3a85ccebcd6258b1c4d1151078e6573349ea
Martin Yeghiazaryan <martineghiazaryan@gmail.com>
2022-11-29 19:22:24 +0100

    Graph composer added

--- graph/compose.py
+++ graph/compose.py
@@ -1,34 +1,43 @@
 # what do we need to do here
+import os
+import re
 import string
 import random
-from graph import Graph, Vertices
+from graph import Graph, Vertex
 
 def get_words_from_text(text_path):
-    with open(text_path, 'r') as f:
-        text = f.read()
+    with open(text_path, 'rb') as file:
+        text = file.read().decode("utf-8") 
 
-        text = ' '.join(text.split()) # this is saying turn whitespread into just spaces
+
+        text = ' '.join(text.split())
         text = text.lower()
-        text = text.translate(str.maketrans('','', string.punctuation))
-    
+        text = text.translate(str.maketrans('', '', string.punctuation))
+
     words = text.split()
+
+    words = words[:1000]
+
     return words
 
 def make_graph(words):
     g = Graph()
-
-    previous_word = None
-
+    prev_word = None
+    # for each word
     for word in words:
+        # check that word is in graph, and if not then add it
         word_vertex = g.get_vertex(word)
 
-        if previous_word:
-            previous_word.increment_edge(word_vertex)
-        
-        previous_word = word_vertex
+        # if there was a previous word, then add an edge if does not exist
+        # if exists, increment weight by 1
+        if prev_word:  # prev word should be a Vertex
+            # check if edge exists from previous word to current word
+            prev_word.increment_edge(word_vertex)
+
+        prev_word = word_vertex
 
     g.generate_probability_mappings()
-
+    
     return g
 
 def compose(g, words, length=50):
@@ -39,16 +48,14 @@ def compose(g, words, length=50):
         word = g.get_next_word(word)
 
     return composition
-def main():
-    # step 1: get words from text
-    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
-    # step 2: make a graph using those words
-    # step 3: get the next word for x number of words (defined by user)
-    # step 4: show the user!
-    g = make_graph(words)
 
+def main():
+    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+        
+    g = make_graph(words)
     composition = compose(g, words, 100)
-    return ' '.join(composition)
+    print(' '.join(composition))
+
 
 if __name__ == '__main__':
-    print(main())
+    main()
